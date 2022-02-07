2b1761ac467f124282d11f7fc91aabeb4cb070da
Angelo <cupertinoangelo13@gmail.com>
2022-02-06 23:43:07 -0300

    :recycle: refactor: Iniciando permutacao

--- src/main.c
+++ src/main.c
@@ -2,6 +2,51 @@
 #include <stdio.h>
 #include <time.h>
 
+/*void troca(int vetor[], int i, int j)
+{
+    int aux = vetor[i];
+    vetor[i] = vetor[j];
+    vetor[j] = aux;
+}
+*/
+
+void permuta(int tamVetor, int vetorDeRota[tamVetor])
+{
+    int aux[tamVetor], i;
+    for(i=0;i<tamVetor;i++){
+        aux[i]=0;
+    }
+
+    i= 0;
+    while (i<tamVetor){
+        if  (aux[i] < i){
+            if (i%2){
+
+                int posicao = aux[i];
+                int auxTroca = vetorDeRota[posicao];
+                vetorDeRota[posicao] = vetorDeRota[i];
+                vetorDeRota[i] = auxTroca;
+
+                for(int j = 0; j < tamVetor;j++){
+                    printf("%d", vetorDeRota[i]);
+                }
+            }else{
+                int posicao = 0;
+                int auxTroca = vetorDeRota[posicao];
+                vetorDeRota[posicao] = vetorDeRota[i];
+                vetorDeRota[i] = auxTroca;
+
+                aux[i] += 1;
+                i = 0;
+            }
+        }else{
+            aux[i]=0;
+            i += 1;
+        }
+    }
+}
+
+
 int main(){
 
     clock_t start, end;
@@ -9,7 +54,7 @@ int main(){
     start = clock();
 
     FILE *fp;
-    fp = fopen("test.txt","r");
+    fp = fopen("testeGermano.txt","r");
 
     int nCidades, cargaCaminhao;
 
--- src/permuta.c
+++ src/permuta.c
@@ -1,34 +1,81 @@
-#include <stdio.h>
-
-void troca(int vetor[], int i, int j)
+void permuta(int tamVetor, int vetorDeRota[tamVetor])
 {
-	int aux = vetor[i];
-	vetor[i] = vetor[j];
-	vetor[j] = aux;
+//0 1 2 0 3 0 4 5 0
+//tamVetor = nCidades  + qtdCaminhoes + 1
+    int aux[tamVetor], i;
+    for(i=0;i<tamVetor;i++){
+        aux[i]=0;
+    }
+    i= 0;
+    while (i<tamVetor){
+        if  (aux[i] < i){
+            if (i%2){
+                int posicao = aux[i];
+                int auxTroca = vetorDeRota[posicao];
+                vetorDeRota[posicao] = vetorDeRota[i];
+                vetorDeRota[i] = auxTroca;
+
+                for(int j = 0; j < tamVetor;j++){
+                    printf("%d", vetorDeRota[i]);
+                }
+
+            }else{
+                int posicao = 0;
+                int auxTroca = vetorDeRota[posicao];
+                vetorDeRota[posicao] = vetorDeRota[i];
+                vetorDeRota[i] = auxTroca;
+
+                aux[i] += 1;
+                i = 0;
+            }
+        }else{
+            aux[i]=0;
+            i += 1;
+        }
+    }
 }
 
-void permuta(int vetor[], int inf, int sup)
-{
-	if(inf == sup){
-		for(int i = 0; i <= sup; i++)
-			printf("%d ", vetor[i]);
-		printf("\n");
+int vetorDeRota[nCidades+qtdCaminhoes+1], i;
+for (i=0;i<nCidades+qtdCaminhoes+1;i++){
+	if (i<=nCidades){
+		vetorDeRota[i]=i;
+	}else{
+		vetorDeRota[i]=0;
 	}
-	else{
-		for(int i = inf; i <= sup; i++){
-			troca(vetor, inf, i);
-			permuta(vetor, inf + 1, sup);
-			troca(vetor, inf, i); // backtracking
-		}
-	}
 }
 
-int main()
-{
-	int v[] = {1, 2, 3, 4};
-	int tam_v = sizeof(v) / sizeof(int);
+//-----------------------------------Codigo antigo-----------------------------------
 
-	permuta(v, 0, tam_v - 1);
+/*for (int qtd = 0; qtd < qtdCaminhoes; qtd++){
+        int stop = 0, atual = 0, maisProxima, cargaAtual = cargaCaminhao;
+        float menor;
+        printf("%d", 0);
+        while (stop == 0){
+            int  aux = 0;
+            for (int j = 1; j < nCidades; j++){
+                if (j != atual){
+                    if (verifcarCidade[j] == 0){ //1 - ja preenchida, 0 - ninguem foi la
+                        if(aux == 0){
+                            menor = matCidades[atual][j];
+                            maisProxima = j;
+                            aux = 1;
+                        }
+                        if (matCidades[atual][j] < menor){
+                            menor = matCidades[atual][j];
+                            maisProxima = j;
+                        }
+                    }
+                }
+            }
 
-	return 0;
-}
+            if (cargaAtual >= demandaCidade[maisProxima]){
+                verifcarCidade[maisProxima] = 1;
+                cargaAtual = cargaAtual - demandaCidade[maisProxima];
+                atual = maisProxima;
+                printf("%d", maisProxima);
+            }else{
+                stop = 1;
+            }
+        }
+    }
+    printf("%d", 0);*/
